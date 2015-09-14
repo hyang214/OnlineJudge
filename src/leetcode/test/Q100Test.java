547b509e9f313a452ddded0c36c7fbbd21195005
AozakiTouko <yanghaoasian@outlook.com>
2015-09-14 21:46:29 +0800

    Solve 2 question

---
+++ src/leetcode/test/Q100Test.java
@@ -0,0 +1,21 @@
+package leetcode.test;
+
+import static org.junit.Assert.*;
+import leetcode.easy.Q100;
+
+import org.junit.Test;
+
+/**
+ * author: Hao 
+ * date:Sep 14, 2015
+ * time:8:16:25 PM
+ * purpose:
+ */
+public class Q100Test {
+
+	@Test
+	public void test() {
+		Q100 q100 = new Q100();
+	}
+
+}
---
+++ src/leetcode/util/TreeNode.java
@@ -0,0 +1,13 @@
+package leetcode.util;
+/**
+ * author: Hao 
+ * date:Sep 14, 2015
+ * time:7:59:08 PM
+ * purpose:
+ */
+public class TreeNode {
+	public	int val;
+    public	TreeNode left;
+    public	TreeNode right;
+    public	TreeNode(int x) { val = x; }
+}
