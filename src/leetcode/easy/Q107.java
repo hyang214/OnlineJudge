6bdff869d17378ee5d40e35ec6add958a7b55ef8
hao-cn <yanghaoasian@outlook.com>
2015-09-15 23:14:27 +0800

    107 solved

--- src/leetcode/easy/Q107.java
+++ src/leetcode/easy/Q107.java
@@ -1,4 +1,11 @@
 package leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import leetcode.util.TreeNode;
+
 /**
  * author: Hao 
  * date:Sep 15, 2015
@@ -6,5 +13,32 @@ package leetcode.easy;
  * purpose:
  */
 public class Q107 {
-
+    public List<List<Integer>> levelOrder(TreeNode root) {
+    	List<List<Integer>> levelList = new ArrayList<List<Integer>>();
+    	if(root == null)
+    		return levelList;
+    	LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+        	ArrayList<Integer> list = new ArrayList<Integer>();
+        	LinkedList<TreeNode> newQueue = new LinkedList<TreeNode>();
+        	while(!queue.isEmpty()){
+        		TreeNode tn = queue.poll();
+        		list.add(tn.val);
+        		if(tn.left != null)
+        			newQueue.add(tn.left);
+        		if(tn.right != null)
+        			newQueue.add(tn.right);
+        	}
+        	queue = newQueue;
+        	levelList.add(list);
+        }
+        List<List<Integer>> rvervselevelList = new ArrayList<List<Integer>>();
+        for(int i = levelList.size() - 1 ; i > -1 ; i --){
+        	rvervselevelList.add(levelList.get(i));
+        }
+        
+        return rvervselevelList;
+    }
 }
