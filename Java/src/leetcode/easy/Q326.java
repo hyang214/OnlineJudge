96994e5cfaf3e51984440ba03a641d03d5a4a586
hao-cn <yanghaoasian@outlook.com>
2016-02-12 20:32:40 +0800

    New solution for 326

--- Java/src/leetcode/easy/Q326.java
+++ Java/src/leetcode/easy/Q326.java
@@ -15,12 +15,27 @@ public class Q326 {
         //return Math.floor(res) == Math.ceil(res);
         /** in order to judge that tow double values are equal or not, we cannot use '==', 
          * we should use |a - b| < Math.e **/
-        double d = Math.abs(res - Math.round(res));
-        return d < Double.MIN_NORMAL;
+        return Math.abs(res - Math.round(res)) < 0.0000000001;
+    }
+    
+    public boolean isPowerOfThree01(int n) {
+    	if(0 == n)
+            return false;
+        double res = Math.log10(n)/Math.log10(3);
+        return Math.floor(res) == Math.ceil(res);
+    }
+    
+    /** the best method I have seen **/
+    public boolean isPowerOfThree02(int n) {
+    	// 1162261467 is 3^19,  3^20 is bigger than int  
+	    return ( n>0 &&  1162261467%n==0);
     }
     
     public static void main(String[] args) {
 		Q326 q326 = new Q326();
 		q326.isPowerOfThree(243);
+		double d1 = 0.1;
+		d1 += 0.1;d1 += 0.1;d1 += 0.1;d1 += 0.1;d1 += 0.1;d1 += 0.1;d1 += 0.1;d1 += 0.1; // 连着加了8次
+		System.out.println(d1);  
 	}
 }
