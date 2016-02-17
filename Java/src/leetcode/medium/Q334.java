cff6911ea0c8c4b3f1283b1443336a98055296ab
AozakiTouko <yanghaoasian@outlook.com>
2016-02-17 15:38:25 +0800

    Q334 todo

---
+++ Java/src/leetcode/medium/Q334.java
@@ -0,0 +1,12 @@
+package leetcode.medium;
+/**
+ * author: Hao 
+ * date:Feb 17, 2016
+ * time:3:17:04 PM
+ * purpose: Increasing Triplet Subsequence
+ */
+public class Q334 {
+    public boolean increasingTriplet(int[] nums) {
+        
+    }
+}
